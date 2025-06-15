544099e56618935dbb8f7db348b47e930ad36112
Tom Gallagher <tga62@uclive.ac.nz>
2025-06-15 16:56:00 +1200

    update readme and allow view_csv to be called with a command line argument

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Real-time **EMG (+ EEG soon)** capture and labelled data saving and plotting to 
 
 ---
 
-## 1 . Quick Start (desktop demo)
+## 1 . Quick Start (view example emg data)
 
 ```bash
 # Clone the repo
@@ -20,3 +20,22 @@ pip install -r requirements.txt   # numpy, matplotlib, tk ...
 
 # Plot the supplied demo recording
 python view_csv.py
+```
+
+## 2 . EMG Recording
+
+To record new emg data, connected the syncstation to the computer using a LAN connection,
+and connect the MUOVI device to the syncstation wirelessly. 
+With the hardware connected, run the file: timer.py
+```bash
+python timer.py
+```
+You will be prompted for the experiment parameters, then the recording experiment will run, saving the data based 
+on the provided subject ID and date. This data will be saved in both csv and hdf5 format, in the /emg_data folder.
+
+## 3 . Viewing Your Data
+To view your data, run view_csv with the filename you would like to view. This can be configured at the top of the view.csv file to plot a single channel but plots all 32 EMG channels by default.
+```bash
+python view_csv filename.csv
+```
+Filenames are of the form emg_data_ID{id}_{date}_M{movement}R{rep}.csv, where the date is in dd-mm format.
--- view_csv.py
+++ view_csv.py
@@ -1,24 +1,18 @@
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-
-FILENAME = "example_data.csv"           # Put the filename here and run this python file to view.
-SINGLE_CHANNEL_MODE = False               # Set to True to see just the first channel, set to False to see all channels
-AMPLITUDE = 0.7                          # You can adjust the amplitude here if the data goes off the edges of the graph
+DEFAULT_FILENAME = "example_data.csv"  # default example filename
+SINGLE_CHANNEL_MODE = False            # Set to True to see just the first channel, set to False to see all channels
+AMPLITUDE = 0.7                        # You can adjust the amplitude here if the data goes off the edges of the graph
 
 def plot_file(filename):
-    """
-    Loops through all CSV files in a folder, displaying each for a given number of seconds.
-    """
-
     file_path = os.path.join("emg_data", "csv", filename)
-
     data = np.loadtxt(file_path, delimiter=',')
     data = data.transpose()
-
 
     plt.clf()
     fig, axes = plt.subplots(nrows=32, ncols=1, figsize=(16, 16), sharex=True)
@@ -30,33 +24,23 @@ def plot_file(filename):
         axes[j].set_xticks([])
         axes[j].plot(emg_signal, label=f'Channel {j + 1}')
 
-
-
     plt.show()
 
 def plot_channel(filename, channel=0):
-    """
-    Plots the given emg data file.
-    """
-
     file_path = os.path.join("emg_data", "csv", filename)
-
     data = np.loadtxt(file_path, delimiter=',')
     data = data.transpose()
 
     plt.clf()
     plt.figure(figsize=(10, 6))
     plt.ylim((-1 * AMPLITUDE, AMPLITUDE))
-
     plt.plot(data[channel])
-
-
-
     plt.show()
 
 if __name__ == '__main__':
+    filename = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_FILENAME
 
     if SINGLE_CHANNEL_MODE:
-        plot_channel(FILENAME)
+        plot_channel(filename)
     else:
-        plot_file(FILENAME)
+        plot_file(filename)
